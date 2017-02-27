b3f76258ddbb5ab15868dcee9374b40fc726f326
Kyle Qian <cqian@stanford.edu>
2017-02-27 18:38:45 +0000

    Changed 'folder' to 'dir' and setup assembler

--- sample_vibes_settings.py
+++ sample_vibes_settings.py
@@ -7,6 +7,6 @@ vibes_settings = {
   "YT_CHANNEL_ID": "",
   "YT_API_KEY": "",
 
-  # By default saves everything in a main folder on the desktop called Vibes
-  "MAIN_FOLDER_PATH": os.path.expanduser("~/Desktop/Vibes")
+  # By default saves everything in a main directory on the desktop called Vibes
+  "MAIN_DIR_PATH": os.path.expanduser("~/Desktop/Vibes")
 }
--- vibes.py
+++ vibes.py
@@ -5,6 +5,7 @@ import errno
 import json
 import pafy
 import pdb
+import shutil
 from time import sleep
 from vibes_settings import vibes_settings
 
@@ -13,48 +14,54 @@ from vibes_settings import vibes_settings
 # TODO: %s -> string.format?
 # TODO: Limited to 50 playlists for now
 # TODO: What if video not available
-# TODO: THUMBNAILS!
+# TODO: Create temporary staging directory where new audio files are downloaded along with artwork, combine them there, then
+# TODO: http://stackoverflow.com/questions/23301256/python-mutagen-add-cover-photo-album-art-by-url
+# TODO: After that, move to main directory and clean up staging, remove staging directory at the very end
+# TODO: https://img.youtube.com/vi/<insert-youtube-video-id-here>/maxresdefault.jpg
+# TODO: Hacky .replace?
 class Vibes(object):
   def __init__(self):
     self.YT_API_KEY = vibes_settings["YT_API_KEY"]
     self.YT_CHANNEL_ID = vibes_settings["YT_CHANNEL_ID"]
     self.PLAYLISTS_URL = "https://www.googleapis.com/youtube/v3/playlists?key=%s&part=snippet&maxResults=50&channelId=%s" % (self.YT_API_KEY, self.YT_CHANNEL_ID)
     self.PLAYLISTITEMS_URL = "https://www.googleapis.com/youtube/v3/playlistItems?key=%s&part=snippet&maxResults=50&playlistId=" % self.YT_API_KEY
-    self.MAIN_FOLDER_PATH = vibes_settings["MAIN_FOLDER_PATH"]
-    self.LIBRARY_PATH = self.MAIN_FOLDER_PATH + "/lib.json"
+    self.MAIN_DIR_PATH = vibes_settings["MAIN_DIR_PATH"]
+    self.LIBRARY_PATH = self.MAIN_DIR_PATH + "/lib.json"
     pafy.set_api_key(self.YT_API_KEY)
 
-  def audio_download(self):
-    # Creates main folder
-    self.__create_folder(self.MAIN_FOLDER_PATH, main=True)
+  def pull(self):
+    # Create main directory
+    self.__create_dir(self.MAIN_DIR_PATH, dirtype="main")
 
-    # Loads library
+    # Load library
     library = json.load(open(self.LIBRARY_PATH, "r"))
     assert isinstance(library, dict)
 
-    # Gets all playlists
+    # Get all playlists
     playlists = requests.get(self.PLAYLISTS_URL).json()["items"]
 
     for p in playlists:
       playlist_id = p["id"]
       playlist_title = p["snippet"]["title"]
 
-      # Creates folder for playlist
-      curr_playlist_path = self.MAIN_FOLDER_PATH + "/" + playlist_title
-      self.__create_folder(curr_playlist_path)
+      # Create directory for playlist, plus the containing assembler directory
+      curr_playlist_path = self.MAIN_DIR_PATH + "/" + playlist_title
+      curr_assembler_path = curr_playlist_path + "/assembler"
+      self.__create_dir(curr_playlist_path)
+      self.__create_dir(curr_assembler_path, dirtype="assembler")
       # log = open(curr_playlist_path + "/log.txt", "a")
 
-      # Adds playlist to library
+      # Add playlist to library
       if playlist_id not in library:
         library[playlist_id] = {
-                                  "title": playlist_title,
-                                  "items": {}
-                               }
+          "title": playlist_title,
+          "items": {}
+        }
 
       counter = 0
       page_token = ""
       while True:
-        # Gets page of videos in playlist
+        # Get page of videos in playlist
         videos_json = requests.get(self.PLAYLISTITEMS_URL + playlist_id + "&pageToken=" + page_token).json()
         page_token = videos_json["nextPageToken"] if "nextPageToken" in videos_json else ""
 
@@ -66,12 +73,11 @@ class Vibes(object):
           # Skip if already in library
           if video_id in library[playlist_id]["items"]: continue
 
-          # Create Pafy object, extract audio, and download audio
+          # Create Pafy object and grab audio
           pafy_object = pafy.new(video_id, basic=False)
-          audio = pafy_object.getbestaudio(preftype="m4a")
-          # remux_audio necessary to play on iTunes
-          # Requires ffmpeg installed
-          audio.download(quiet=True, filepath=curr_playlist_path, remux_audio=True)
+
+          # Assemble and download file and metadata
+          self.__process_pafy(pafy_object)
 
           # Update library file
           library[playlist_id]["items"][video_id] = video_title
@@ -89,20 +95,63 @@ class Vibes(object):
 
         # No more videos
         if page_token == "": break
+
+      # Delete assembler directory
+      shutil.rmtree(curr_assembler_path)
+
       # log.close()
 
-  def __create_folder(self, path, main=False):
+  def __process_pafy(self, pafy_object):
+    # Get audio stream from Pafy
+    audio = pafy_object.getbestaudio(preftype="m4a")
+
+    # Download audio file to assembler directory
+    # remux_audio necessary to play on iTunes
+    # >>> Requires ffmpeg installed <<<
+    file_path = audio.download(quiet=True, filepath=curr_assembler_path, remux_audio=True)
+
+    # Assemble audio file with metadata
+    pass
+
+    # Move assembled file into parent directory and clear assembler
+    shutil.move(file_path, file_path.replace("/assembler"))
+    self.__clear_dir(curr_assembler_path)
+
+  # Creates directory; defaults to playlist
+  def __create_dir(self, path, dirtype="playlist"):
     try:
       os.makedirs(path)
     except OSError as exception:
       if exception.errno != errno.EEXIST:
         raise
 
-    # Create library file if it doesn't yet exist
-    if main and os.path.isfile(self.LIBRARY_PATH) == False:
+    # Default playlist dir
+    if dirtype == "playlist":
+      pass
+
+    # Main dir including library file
+    elif dirtype == "main":
+      if os.path.isfile(self.LIBRARY_PATH) == False:
         with open(self.LIBRARY_PATH, "w") as lib:
           lib.write("{}")
 
+    # Assembler dir, makes sure it's empty
+    elif dirtype == "assembler":
+      self.__clear_dir(path)
+
+    # Unknown type
+    else:
+      raise
+
+  # Clears directory
+  def __clear_dir(self, path):
+    try:
+      shutil.rmtree(path)
+      os.makedirs(path)
+    except OSError as exception:
+      if exception.errno != errno.EEXIST:
+        raise
+
 if __name__ == '__main__':
   V = Vibes()
-  V.audio_download()
+  V.pull()
